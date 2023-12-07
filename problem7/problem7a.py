e50b3561904c082634099b29c979e41cbda242f1
Justin Delgado <justind4@usf.edu>
2023-12-07 13:53:49 -0500

    problem 7 solved (yay!!)

---
+++ problem7/problem7a.py
@@ -0,0 +1,45 @@
+from collections import Counter
+
+hand_array = {
+    "five_of_kind" : [],
+    "four_of_kind": [],
+    "full_house" : [],
+    "three_of_kind": [],
+    "two_pair": [],
+    "one_pair": [],
+    "high_card": []
+}
+
+cards = []
+with open("input.txt") as f:
+    cards = list(map(lambda x: [x[0], int(x[1])], [hand.strip().split() for hand in f.readlines()]))
+
+for hand in cards:
+    hand[0]= hand[0].replace('T', 'a').replace('J', 'b').replace('Q', 'c').replace('K','d').replace('A','e')
+    counted_freq = Counter(hand[0])
+    two_most_common = counted_freq.most_common(2)
+    if two_most_common[0][1] == 5:
+        hand_array["five_of_kind"].append(hand)
+    elif two_most_common[0][1] == 4:
+        hand_array["four_of_kind"].append(hand)
+    elif two_most_common[0][1] == 3 and two_most_common[1][1] == 2:
+        hand_array["full_house"].append(hand)
+    elif two_most_common[0][1] == 3:
+        hand_array["three_of_kind"].append(hand)
+    elif two_most_common[0][1] == 2 and two_most_common[1][1] == 2:
+        hand_array["two_pair"].append(hand)
+    elif two_most_common[0][1] == 2:
+        hand_array["one_pair"].append(hand)
+    else:
+        hand_array["high_card"].append(hand)
+
+cur_rank = len(cards)
+
+ans = 0
+
+for hand_type in hand_array.keys():
+    for hand in sorted(hand_array[hand_type], key=lambda x: x[0], reverse=True):
+        ans += hand[1] * cur_rank
+        cur_rank -= 1
+
+print(ans)
